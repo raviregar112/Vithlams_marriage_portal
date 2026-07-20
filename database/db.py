392542a1721a9b40291b9d734c313ad42ecd7cb7
= <raviregar112@gmail.com>
2026-07-20 14:38:28 +0530

    Update db.py

--- database/db.py
+++ database/db.py
@@ -1,15 +1,14 @@
 import os
 import mysql.connector
 
-
 def get_connection():
 
     conn = mysql.connector.connect(
         host=os.environ.get("MYSQLHOST"),
+        port=int(os.environ.get("MYSQLPORT")),
         user=os.environ.get("MYSQLUSER"),
         password=os.environ.get("MYSQLPASSWORD"),
-        database=os.environ.get("MYSQLDATABASE"),
-        port=int(os.environ.get("MYSQLPORT"))
+        database=os.environ.get("MYSQLDATABASE")
     )
 
     return conn
